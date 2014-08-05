9a7d2e9b3c91c1a7f6d97fa6ede3870a71151cf0
Ankur Agarwal <ankur.agarwal@ocado.com>
2014-08-05 12:49:13 +0100

    Adding another unit test to test the deserialized training data

--- ondelabs/test/WholeSystemTest.py
+++ ondelabs/test/WholeSystemTest.py
@@ -3,11 +3,6 @@ Created on 3 Aug 2014
 
 @author: ankur
 '''
-'''2) Load Data '''
-'''3) Train '''
-'''4) Save Model '''
-'''5) Test Model Accuracy '''
-
 import logging
 import unittest
 
@@ -19,13 +14,10 @@ from ondelabs.model.TrainingData import TrainingData
 class WholeSystemTest(unittest.TestCase):
 
     def testSystem(self):
-        trainingData = TrainingData.deserialize()
-
-        if trainingData is None:
-            trainingDAO = DiskTrainingDAO('resource/training.txt')
-            trainingData = trainingDAO.loadData()
-            trainingData.train()
-            trainingData.serialize()
+        trainingDAO = DiskTrainingDAO('resource/training.txt')
+        trainingData = trainingDAO.loadData()
+        trainingData.train()
+        trainingData.serialize()
 
         logging.info('Training has completed')
 
@@ -39,3 +31,17 @@ class WholeSystemTest(unittest.TestCase):
         self.assertEqual(validationResult.getAccuracy(), 100, "Accuracy is incorrect")
         self.assertEqual(validationResult.getTotalPredictions(), validationData.getTotalValidationTestDataPoints(), "Number of predictions doe not equal number of validation tests")
 
+    def testDeserializedModel(self):
+        trainingData = TrainingData.deserialize()
+
+        logging.info('Training data has been loaded')
+
+        validationDAO = DiskValidationDAO('resource/validation.txt')
+        validationData = validationDAO.loadData()
+        validationResult = validationData.validate(trainingData.getClasses(), trainingData.getLexicon())
+
+        logging.info('Validation has completed')
+
+        self.assertIsNotNone(validationResult, "validationResult should not be None")
+        self.assertEqual(validationResult.getAccuracy(), 100, "Accuracy is incorrect")
+        self.assertEqual(validationResult.getTotalPredictions(), validationData.getTotalValidationTestDataPoints(), "Number of predictions doe not equal number of validation tests")
